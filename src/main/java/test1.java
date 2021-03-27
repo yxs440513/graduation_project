98d1fdda474248ac32374ff563131da2de87bc58
yxs66 <1284675135@qq.com>
2021-03-27 15:41:13 +0800

    测试

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,7 +1,5 @@
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("dd");
-        System.out.println("dd");
-        System.out.println("yyyy");
+        System.out.println("dsafdshifhsd");
     }
 }
